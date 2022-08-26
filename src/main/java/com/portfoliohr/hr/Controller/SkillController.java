3124c79f805fb09155b2fa033babea59d92694bd
hirrei86 <95895589+hirrei86@users.noreply.github.com>
2022-08-26 15:12:23 -0300

    actualizacion para deploy

--- src/main/java/com/portfoliohr/hr/Controller/SkillController.java
+++ src/main/java/com/portfoliohr/hr/Controller/SkillController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ap-fronthr.web.app/")
 public class SkillController {
     
    @Autowired
